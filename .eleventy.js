7bcd3ce2e0fdafb79b72fa995f0d95eb851ec4ed
Duncan Sample <439893-dsample@users.noreply.gitlab.com>
2025-10-18 20:10:40 +0100

    Add events calendar

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,18 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation")
 const CleanCSS = require("clean-css");
+const dateFilter = require('nunjucks-date-filter');
 
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(eleventyNavigationPlugin)
 
-  eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addNunjucksFilter("date", dateFilter);
+
+  eleventyConfig.addCollection("events", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("src/ptfa/events/*.md").sort(function(a, b) {
+      return a.date - b.date;
+    });
+  });
 
   eleventyConfig.addFilter("cssmin", function(code) {
     return new CleanCSS({}).minify(code).styles;
---
+++ src/ptfa/events/index.md
@@ -0,0 +1,4 @@
+---
+layout: ptfa-events
+title: Upcoming Events
+---
